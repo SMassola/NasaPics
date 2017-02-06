f25eac57e0f9f5d7029d3e72d7bda854eb91a7b5
Steven Massola <Steven.Massola@gmail.com>
2017-02-06 00:57:28 -0500

    main component

--- frontend/components/entry.jsx
+++ frontend/components/entry.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { hashHistory, Router, Route, IndexRoute } from 'react-router';
+import Main from './main/main';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+const routes = (
+  <Route path="/" component={App}>
+    <IndexRoute component={Main} />
+  </Route>
+);
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<div>NasaPics</div>, root);
+  ReactDOM.render(<Router history={hashHistory} routes={routes} />, root);
 });
