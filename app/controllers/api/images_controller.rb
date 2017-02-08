db0d934ff18ffdb71fba93b70b10672f0ed40c60
Steven Massola <Steven.Massola@gmail.com>
2017-02-08 09:42:52 -0500

    deploying to heroku

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -2,7 +2,7 @@ class Api::ImagesController < ApplicationController
 
   def index
     @images = []
-    url = URI.parse("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=a5e95177da353f58113fd60296e1d250&user_id=24662369@N07&format=json&nojsoncallback=1")
+    url = URI.parse("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=#{ENV['FLICKR_WEB_API_KEY']}&user_id=24662369@N07&format=json&nojsoncallback=1")
     res = Net::HTTP::get(url)
     images = JSON.load(res) || []
     @images = images["photos"]["photo"]
